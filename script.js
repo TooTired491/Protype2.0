61ef8472a5f08e23153edd57f8fcf2fe17ec0697
TooTired491 <Insomniaccat491@gmail.com>
2025-04-27 12:50:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+//About
+let hoverElement1 = document.getElementById('button');
+hoverElement1.addEventListener('mouseenter', function() {
+	hoverElement1.style.opacity = '0.8';
+})
+hoverElement1.addEventListener('mouseleave', function() {
+	hoverElement1.style.opacity = '3';
+});
+//Tours
+let hoverElement2 = document.getElementById('button2'); 
+hoverElement2.addEventListener('mouseenter', function() {
+	hoverElement2.style.opacity = '0.8';
+})
+hoverElement2.addEventListener('mouseleave', function() {
+	hoverElement2.style.opacity = '1';
+});
+//Events
+let hoverElement3 = document.getElementById('button3'); 
+hoverElement3.addEventListener('mouseenter', function() {
+	hoverElement3.style.opacity = '0.8';
+})
+hoverElement3.addEventListener('mouseleave', function() {
+	hoverElement3.style.opacity = '1';
+});
 
